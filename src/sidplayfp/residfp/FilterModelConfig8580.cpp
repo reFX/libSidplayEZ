d2d9b6e7f8da225b3777578479f69169436724b1
Michael Hartmann <michael.hartmann@refx.com>
2024-02-08 05:03:29 -0800

    Adjusted voice voltage ranges to reference

--- src/sidplayfp/residfp/FilterModelConfig8580.cpp
+++ src/sidplayfp/residfp/FilterModelConfig8580.cpp
@@ -123,7 +123,7 @@ FilterModelConfig8580* FilterModelConfig8580::getInstance ()
 
 FilterModelConfig8580::FilterModelConfig8580 () :
 	FilterModelConfig (
-		0.237,	// voice voltage range FIXME measure
+		0.24,	// voice voltage range FIXME measure
 		4.84,	// voice DC voltage FIXME measure
 		22e-9,	// capacitor value
 		9.09,	// Vdd
